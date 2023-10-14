58bac2e18493dbbb816bb5e9034be903df5717ec
LanSang69 <alanisg029@gmail.com>
2023-10-14 09:40:23 -0600

    Changing the form

--- js/login.js
+++ js/login.js
@@ -1,49 +1,17 @@
-const login = document.querySelector(".Login");
-const register = document.querySelector(".Register");
-const reg = document.querySelector(".register");
-const log = document.querySelector(".login");
-const registerBut = document.querySelector(".Main .buttonField .Register button");
-const loginBut = document.querySelector(".Main .buttonField .Login button");
+const box = document.querySelector(".caixa");
+const x = document.getElementById("login");
+const y = document.getElementById("register");
+const z = document.getElementById("btn");
 
-login.addEventListener("click" , () => {
-    log.style.transform="translateX(-350px)";
-    reg.style.transform="translateX(-400px)";
-    loginBut.style.background="#000000";
-    loginBut.style.color="#ffff"
-    registerBut.style.background="#ffff";
-    registerBut.style.color="#000000"
-})
-
-register.addEventListener("click", () => {
-    reg.style.transform="translateX(5px)";
-    log.style.transform="translateX(100px)";
-    loginBut.style.background="#ffff";
-    loginBut.style.color="#000000"
-    registerBut.style.background="#000000";
-    registerBut.style.color="#ffff"
-})
-
-const btn = document.querySelector(".sgnup");
-const inpt = document.querySelectorAll("#input1");
-
-inpt.addEventListener('keypress' , handleInputChange);
-
-const saveToLocalStorage = () => {
-    if(inpt.value) {
-        localStorage.setItem('input1' , inpt.value);
-    }
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+  box.style.height = "680px";
 }
-
-const getFromLocalStorage = () => {
-    inpt.value=localStorage.getItem('input1');
-} 
-
-btn.addEventListener("click" , () => {
-    saveToLocalStorage();
-});
-
-getFromLocalStorage();
-
-function handleInputChange(){
-    saveToLocalStorage();
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0";
+  box.style.height = "480px";
 }
