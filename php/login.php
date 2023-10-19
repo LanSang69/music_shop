a57836eefcd8d4c6b49e2e3ccfcccd23ea228319
LanSang69 <alanisg029@gmail.com>
2023-10-18 19:33:02 -0600

    Fixing login

--- php/login.php
+++ php/login.php
@@ -13,22 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $connection = pg_connect("host=$host port=$port dbname=$dbname user=$username password=$password");
 
     if ($connection) {
-        if ($result) {
-            $row = pg_fetch_assoc($result);
-            $userType = $row["user_type"];
-            
-            // Redireccionar según el tipo de usuario
-            if ($userType === "admin") {
+            if ($username === "admin") {
                 header("Location: dueño/plataforma_admin.html");
-            } elseif ($userType === "empleado") {
+            } elseif ($username === "empleado") {
                 header("Location: empleados/plataforma.html");
             }
-            
             pg_close($connection);
             exit;
-        } else {
-            echo "Error en la consulta SQL: " . pg_last_error();
-        }
     } else {
         echo "Error al conectar a PostgreSQL.";
     }
