a9a01ea1e73320d619f73dbe91e581cc5d9825a5
LanSang69 <alanisg029@gmail.com>
2023-10-25 22:39:48 -0600

    Adding design to login

--- php/login.php
+++ php/login.php
@@ -14,19 +14,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($connection) {
         if ($username === "admin") {
+            // Redirect to the admin page
             header("Location: ../src/dueno/plataforma_admin.html");
+            exit; // Prevent further execution of the script
         } elseif ($username === "empleado") {
+            // Redirect to the employee page
             header("Location: ../src/empleados/plataforma.html");
+            exit; // Prevent further execution of the script
+        } else {
+            $response = array(
+                'success' => false,
+                'message' => 'Credenciales erróneas'
+            );
         }
         pg_close($connection);
-        exit;
     } else {
         $response = array(
             'success' => false,
             'message' => 'Credenciales erróneas'
         );
-        header('Content-Type: application/json');
-        echo json_encode($response);
     }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
 ?>
