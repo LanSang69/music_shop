073f8a193577426fee075396088d8d47b8120874
Alan Sánchez <alanisg029@gmail.com>
2023-11-04 23:07:55 -0600

    Cambiando titulo página

--- home_log.php
+++ home_log.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <Meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MusicStore | La música es tu mejor compañía</title>
+    <title>La Nota Mala | La música es tu mejor compañía</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="css/styles_home.css">
 </head>
